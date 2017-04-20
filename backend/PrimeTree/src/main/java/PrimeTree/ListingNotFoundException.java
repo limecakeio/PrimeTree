c7e6af24582ca9b06286215612fb7187a9f3ad85
Johannes16 <johannes.lotz1@web.de>
2017-04-20 14:29:32 +0200

    First running Server

--- backend/PrimeTree/src/main/java/PrimeTree/ListingNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package PrimeTree;
-
-public class ListingNotFoundException extends Exception {
-
-}
--- backend/PrimeTree/src/main/java/PrimeTree/WrongFormatException.java
+++
@@ -1,9 +0,0 @@
-package PrimeTree;
-
-public class WrongFormatException extends Exception {
-
-	public WrongFormatException(String string) {
-		super(string);
-	}
-
-}
