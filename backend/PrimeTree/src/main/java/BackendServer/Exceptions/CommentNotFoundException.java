a58e14a51423f8f45bdce0336749785a42b67fe5
FlorianKutz1522784 <florian.kutz@t-online.de>
2017-05-24 13:26:57 +0200

    Target weg und geadded

---
+++ backend/PrimeTree/src/main/java/BackendServer/Exceptions/CommentNotFoundException.java
@@ -0,0 +1,9 @@
+package BackendServer.Exceptions;
+
+/**This Exception is thrown if someone tries to access a comment with an id that does not exist as a comment.
+ * 
+ * @author Florian Kutz
+ * */
+public class CommentNotFoundException extends Exception {
+
+}
