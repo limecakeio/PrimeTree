36aebd700232025fe0f961aa3cd27a039592146e
Richard Vladimirskij <admin@limecake.com.au>
2017-06-11 12:14:54 +0200

    Sunday merging

--- frontend/bitKleinanzeigen/src/app/form/elements/image/image.component.ts
+++ frontend/bitKleinanzeigen/src/app/form/elements/image/image.component.ts
@@ -37,11 +37,16 @@ export class ImageFormComponent {
   data : any;
   imagesrc : SafeUrl = '';
   image : SafeStyle;
-  imageFile : any;
+  imageElement: HTMLImageElement;
+
   imgWidth: number;
   imgHeight: number;
-  targetCanvasWidth: "600";
-  targetCanvasHeight: "315";
+  imgSize : string;
+  imgRatio : number;
+  targetCanvasWidth: number = 600;
+  targetCanvasHeight: number = 315;
+  ogRatio: number = 0.525;
+  imageContainerHeight: number;
   imagePreviewContainer: any;
 
 
@@ -54,9 +59,11 @@ export class ImageFormComponent {
     });
   }
 
+  /*
+  * These events enable the user to drag and drop an image file into the uploader
+  */
   private handleEvents() : void {
     this.div = document.querySelector('.image-box');
-
     if (!this.div) {
       return;
     }
@@ -81,51 +88,41 @@ export class ImageFormComponent {
     this.addMulipleEventListener(this.div, 'drop', (event : any) => {
       console.log("DATA TRANSFER", event.dataTransfer);
       this.preloadImage(event.dataTransfer.files[0]);
-
-
-
-
-      // this.fileToBase(this.data.imageAsFile, (base : string) => {
-      //   this.data.imageAsBase = base;
-      //   this.data.imageAsByteArray = this.baseToByte(base);
-      //   let file : File = this.byteToFile(this.data.imageAsByteArray);
-      //   // let str : StreamReader = new StreamReader();
-      //   this.imagesrc = 'data:image/jpeg;base64,' + this.data.imageAsByteArray;
-      //
-      // });
-      // this.fileToByteArray(this.data.imageAsFile, (bytearray : Uint8Array) => {
-      //   // let src : string = String.fromCharCode.apply(null, bytearray);
-      //   this.data.imageAsByteArray = bytearray;
-      //   let ele = document.querySelector('#file-input-image');
-      //   ele.appendChild(this.decodeArrayBuffer(bytearray, () => {
-      //
-      //   }));
-      // });
     });
-
-    /**User clicks on the file-upload*/
-    // this.addMulipleEventListener(this.div, 'click', (event : any) => {
-    //   console.log("DATA TRANSFER", event);
-    //   this.preloadImage(event.dataTransfer.files[0]);
-    // });
   };
 
-  /**Generates and presents an image file from the uploader*/
+  /*
+  * We also allow the user to click and choose a file from their system
+  */
+  imageInput(event : any) {
+    this.preloadImage(event.target.files[0]);
+    let path : string = URL.createObjectURL(this.data.imageAsFile);
+    let reader : FileReader = new FileReader();
+    reader.onloadend = () => {
+      this.image = this.domSanitizer.bypassSecurityTrustStyle('url(' + reader.result + ')');
+    }
+    reader.readAsDataURL(this.data.imageAsFile);
+  }
+
+  /*
+  * Generates and presents a preview from the uploader
+  * @imageFile the file submitted by the uploader
+  */
   private preloadImage(imageFile : File) : void {
-
-    let ImageComponent : ImageFormComponent = this;
-
-    //Set the image's file
-    ImageComponent.imageFile = imageFile;
 
     /*GENERATE IMAGE PREVIEW*/
     let imageResult = new Image();
+    imageResult.src = URL.createObjectURL(imageFile);
 
-
+    let ImageComponent : ImageFormComponent = this; //Binding this to async method
     imageResult.onload = function() {
+      /**Save the image*/
+      ImageComponent.imageElement = imageResult;
       /*Set the images dimensions*/
       ImageComponent.imgWidth = imageResult.width;
       ImageComponent.imgHeight = imageResult.height;
+      /*Set the image's ratio*/
+      ImageComponent.imgRatio = imageResult.height / imageResult.width;
 
       /*Hide the image upload*/
       let imageInputContainer = document.querySelector(".image-input-container");
@@ -138,30 +135,25 @@ export class ImageFormComponent {
       ImageComponent.setImageContainerDimensions();
 
       /*Inject image into preview as a background image*/
-      ImageComponent.imagePreviewContainer.style.backgroundImage = "url('"+imageResult.src+"')";
-
+      ImageComponent.imagePreviewContainer.style.backgroundImage = "url('" + imageResult.src + "')";
       ImageComponent.setDimensionsAndZoomer();
-
     }
-    imageResult.src = URL.createObjectURL(imageFile);
+
   }
 
   zoomImage() : void {
     let rangeSlider = <HTMLInputElement>document.querySelector("#zoom-range");
-    let imageContainer = <HTMLElement>document.querySelector("#file-input-image");
     //Adapt zoom-level to image-container background-image
-    imageContainer.style.backgroundSize = "auto " + rangeSlider.value + "px";
+    this.imgSize = "auto " + rangeSlider.value + "px";
   }
 
   /**Calculates the best position for an image to be displayed within the cropper
   and sets the zommer function accordingly*/
   private setDimensionsAndZoomer() : void {
-    /**Calculate and set initial display size*/
-    let imgRatio = this.imgHeight / this.imgWidth;
+
     let zoomRange = <HTMLInputElement>document.querySelector("#zoom-range");
 
-    /*Get the image's as well as its container's dimensions
-    to calculate the perfect initial display*/
+    /*Get the image container's dimensions to calculate the perfect width*/
     let ipcWidth = this.imagePreviewContainer.clientWidth;
     let ipcHeight = this.imagePreviewContainer.clientHeight;
 
@@ -169,18 +161,18 @@ export class ImageFormComponent {
       /*Center the image position for lanscape images*/
       this.imagePreviewContainer.style.backgroundPosition = "0px 0px";
 
-      if(ipcWidth * imgRatio < ipcHeight) {
-        this.imagePreviewContainer.style.backgroundSize = "auto " + ipcHeight + "px";
+      if(ipcWidth * this.imgRatio < ipcHeight) {
+        this.imgSize = "auto " + ipcHeight + "px";
         zoomRange.min = ipcHeight + "";
         zoomRange.max = this.imgHeight + "";
         zoomRange.value = ipcHeight + "";
       } else {
-        this.imagePreviewContainer.style.backgroundSize = "auto " + (ipcWidth * imgRatio) + "px";
-        zoomRange.min = ipcWidth * imgRatio + "";
+        this.imgSize = "auto " + (ipcWidth * this.imgRatio) + "px";
+        zoomRange.min = ipcWidth * this.imgRatio + "";
         zoomRange.max = this.imgWidth + "";
-        zoomRange.value = ipcWidth * imgRatio + "";
+        zoomRange.value = ipcWidth * this.imgRatio + "";
       }
-    } else {
+    } else { //TODO...do we really want to allow this? There might be a better way!
       /*Center the image position for profile/square images*/
       this.imagePreviewContainer.style.backgroundPosition = ipcWidth/2 - this.imgWidth/4 + "px 0px";
       this.imagePreviewContainer.style.backgroundSize = "auto " + ipcHeight + "px";
@@ -190,9 +182,11 @@ export class ImageFormComponent {
     }
   }
 
-  /**Returns true if the current image's orientation is landscape*/
+  /*
+  * Returns true if the current image's orientation is landscape
+  */
   private isLandscape() : any {
-    if(this.imgWidth > this.imgHeight) {//If it's not landscape it's either portrait or square
+    if(this.imgWidth > this.imgHeight) {
       return true;
     }
     return false;
@@ -201,7 +195,7 @@ export class ImageFormComponent {
   /**Sets the image container to the OpenGraph dimension of 1:0.525*/
   private setImageContainerDimensions() : void {
     let resultImageContainer = <HTMLElement>document.querySelector(".result-image-container");
-    resultImageContainer.style.height = resultImageContainer.clientWidth * 0.525 + "px";
+    this.imageContainerHeight = resultImageContainer.clientWidth * this.ogRatio;
   }
 
   /**Moves an image into the direction as provided by the String parameter up, down, left or right*/
@@ -221,7 +215,9 @@ export class ImageFormComponent {
     }
   }
 
-  /**Resets the image container's contents to their initial state*/
+  /*
+  * Resets the image container's contents to their initial state
+  */
   public resetImagePosition() {
     this.setDimensionsAndZoomer();
   }
@@ -239,155 +235,43 @@ export class ImageFormComponent {
   }
 
   public captureImage() : void {
+    //The image container is fluid, we need to adjust the image to reflect the current position and size
+    //in order to draw it correctly onto the canvas
+    let xPos = parseFloat(this.imagePreviewContainer.style.backgroundPositionX);
+    let yPos = parseFloat(this.imagePreviewContainer.style.backgroundPositionY);
 
-    let ImageComponent : ImageFormComponent = this;
+    let dimensionFactor = this.targetCanvasHeight - this.imageContainerHeight;
+     //Filter out the first size then convert and add the factor
+    let backgroundHeight = parseFloat(this.imgSize.replace(/^\S+/, "")) + dimensionFactor;
+    let backgroundWidth = backgroundHeight / this.imgRatio;
 
-    const fr = new FileReader();
-    fr.readAsDataURL(this.imageFile);
+    //Create a canvas for the image
+    let canvas = document.createElement("canvas");
+    canvas.setAttribute("width", this.targetCanvasWidth + "px");
+    canvas.setAttribute("height", this.targetCanvasHeight + "px");
+    canvas.setAttribute("id", "capture-canvas");
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage(this.imageElement, xPos, yPos, backgroundWidth, backgroundHeight);
 
-    fr.onloadend = function() {
-      //Create a canvas for the image
-      let canvas = document.createElement("canvas");
-      canvas.setAttribute("width", ImageComponent.targetCanvasWidth);
-      canvas.setAttribute("height", ImageComponent.targetCanvasHeight);
-      canvas.setAttribute("id", "capture-canvas");
-      let ctx = canvas.getContext('2d');
+    //Save the canvas image
+    this.data.imageAsFile = canvas.toDataURL();
+    console.log("Final image result as base64", this.data.imageAsFile);
 
-      //Reflect the state of the image
-      let imgRatio = ImageComponent.imgWidth / ImageComponent.imgHeight;
-      let xPos = ImageComponent.imagePreviewContainer.style.backgroundPositionX;
-      let yPos = ImageComponent.imagePreviewContainer.style.backgroundPositionY;
-      let height;
-      let width;
-    }
-
+    //Display the result to the user
+    this.imagePreviewContainer.appendChild(canvas);
   }
 
-  input(event : any) {
-    this.data.imageAsFile = event.target.files[0];
-    this.preloadImage(event.target.files[0]);
-    let path : string = URL.createObjectURL(this.data.imageAsFile);
-    let reader : FileReader = new FileReader();
-    reader.onloadend = () => {
-      console.log("FILE READER READ IMAGE");
-      this.image = this.domSanitizer.bypassSecurityTrustStyle('url(' + reader.result + ')');
-    }
-    reader.readAsDataURL(this.data.imageAsFile);
-    console.log(this.data.imageAsFile, path);
-    // this.image = this.domSanitizer.bypassSecurityTrustStyle('url(' + path + ')');
-  }
 
-  // http://stackoverflow.com/questions/4564119/javascript-convert-byte-to-image
-  decodeArrayBuffer(buffer : any, onLoad : any) : HTMLImageElement {
-    var mime;
-    // var a = new Uint8Array(buffer);
-    var a = buffer;
-    var nb = a.length;
-    if (nb < 4)
-        return null;
-    var b0 = a[0];
-    var b1 = a[1];
-    var b2 = a[2];
-    var b3 = a[3];
-    if (b0 == 0x89 && b1 == 0x50 && b2 == 0x4E && b3 == 0x47)
-        mime = 'image/png';
-    else if (b0 == 0xff && b1 == 0xd8)
-        mime = 'image/jpeg';
-    else if (b0 == 0x47 && b1 == 0x49 && b2 == 0x46)
-        mime = 'image/gif';
-    else
-        return null;
-    var binary = "";
-    for (var i = 0; i < nb; i++)
-        binary += String.fromCharCode(a[i]);
-    var base64 = window.btoa(binary);
-    var image = new Image();
-    image.onload = onLoad;
-    image.src = 'data:' + mime + ';base64,' + base64;
-
-    return image;
-}
-
-  submitImage(event : any) : void {
-    if (event === null || event.target === null || event.target.files === null) {
-      return;
-    }
-    let file = event.target.files[0];
-    let reader : FileReader = new FileReader();
-    // reader.onload = this.readerOnload.bind(this);
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      this.model.imageAsByteArray = this.baseToByte(reader.result);
-    }
-    // this.model.imageObj = file;
-  }
-
-  private fileToBase(file : File, callback : any) : void {
-    let reader : FileReader = new FileReader();
-    // file.
-    // console.log(file);
-
-    reader.readAsDataURL(file);
-    reader.onloadend = () => {
-      callback(reader.result);
-    }
-  }
-
-  private fileToByteArray(file : File, callback: any) : void {
-    let reader : FileReader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = () => {
-      callback(new Uint8Array(reader.result));
-    }
-  }
-
-  private readerOnload(e : any) {
-    let reader = e.target;
-    this.imagesrc = reader.result;
-    this.model.image = this.imagesrc;
-  }
-
-  private baseToByte(base : string) : Uint8Array {
-    // base = base.replace(/^data:image\/(png|jpg|jpeg);base64/‌​, '');
-    // base = base.replace('data:image/','');
-    // base = base.replace(/png|jpg|jpeg/, '');
-    // base = base.replace(';base64,', '');
-    base = base.replace(/^data:image\/(jpg|png|jpeg);base64,/, '');
-    let byteCharacters = window.atob(base);
-    let length = byteCharacters.length;
-    let byteArray : Uint8Array = new Uint8Array(new ArrayBuffer(length));
-    for (let i = 0; i < length; i++) {
-      byteArray[i] = byteCharacters.charCodeAt(i);
-    }
-    return byteArray;
-  }
-
-  ngOnInit() : void {
-    this.handleEvents();
-  }
 
   ngAfterViewInit() : void {
     this.imagePreviewContainer = <HTMLElement>document.querySelector("#file-input-image");
   }
 
-  private byteToFile(byteArray : Uint8Array[]) : File {
-    let file : File = new File(byteArray, 'abcdef' + '.jpg', {
-      type: 'image/jepg'
-    });
-    return file;
-  }
-
-  private byteToBase(byteArray : Uint8Array) : string {
-    let base : string = 'data:image/jpeg;base64,';
-    let binary : string = '';
-    const length : number = byteArray.length;
-    for (let i = 0; i < length; i++) {
-      binary += String.fromCharCode(byteArray[i]);
-    }
-    return base + window.btoa(binary);
-  }
-
   /**Handle events*/
+
+  ngOnInit() : void {
+    this.handleEvents();
+  }
 
   /**Listen to the window to adapt image container on changes*/
   @HostListener('window:resize', ['$event'])
@@ -395,7 +279,4 @@ export class ImageFormComponent {
     this.setImageContainerDimensions();
     this.setDimensionsAndZoomer();
   }
-
-
-
 }
